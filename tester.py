42ca896d4587f66e4d5d1a291808fead0d39ddd3
juliemarble <julie.yeo06@gmail.com>
2025-11-16 16:31:47 +0900

    Add distance and triangle area calculations

--- tester.py
+++ tester.py
@@ -1,7 +1,7 @@
 from geo.utils import distance, triangle_area
 
-p1 = (0,0)
-p2 = (3,4)
+p1 = (0, 0)
+p2 = (3, 4)
 
-print(distance(p1,p2))
-print(triangle_area(10,5))
+print(distance(p1, p2))
+print(triangle_area(10, 5))
