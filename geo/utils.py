8c472eb5de0c8ecb1a19921a03b89ced69d77132
juliemarble <julie.yeo06@gmail.com>
2025-11-16 16:25:10 +0900

    Update utils.py

--- geo/utils.py
+++ geo/utils.py
@@ -1,7 +1,8 @@
 import math
 
-def distance(p1,p2):
-  return math.dist(p1,p2)
+def distance(p1, p2):
+    return math.dist(p1, p2)
 
 def triangle_area(base, height):
-  return base* height /2
+    return base * height / 2
+
